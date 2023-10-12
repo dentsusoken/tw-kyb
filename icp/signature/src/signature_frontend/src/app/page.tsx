c6863a5b39e1ee2975e145914a33ce130411a052
Yasuo Higa <higayasuo@gmail.com>
2023-10-12 17:49:01 +0900

    Add google auth to signature app

--- icp/signature/src/signature_frontend/src/app/page.tsx
+++ icp/signature/src/signature_frontend/src/app/page.tsx
@@ -1,71 +1,19 @@
 'use client';
 
-import { useHome } from '@/hooks/useHome';
-import { uint8ArrayToHex } from '@/utils/hexUtils';
-
-const textareaStyles = 'border-2 border-blue-100 rounded-xl w-96';
-const buttonStyles = 'bg-blue-200 rounded-lg px-4 py-1';
+import { useFirebaseAuth } from '@/hooks/useFirebaseAuth';
+import { Signature } from '@/components/Signature';
+import { FirebaseAuth } from '@/components/FirebaseAuth';
 
 export default function Home() {
-  const {
-    publicKey,
-    hash,
-    signature,
-    verifyMessage,
-    onMessageChange,
-    onSignClick,
-    onVerifyClick,
-    error,
-  } = useHome();
+  const { user } = useFirebaseAuth();
+
   return (
     <main className="flex min-h-screen flex-col items-center p-2">
-      <p className="pt-4">
-        <label htmlFor="message">Message:</label>
-        <br />
-        <textarea
-          id="message"
-          className={textareaStyles}
-          onChange={onMessageChange}
-        ></textarea>
-        <br />
-        <label htmlFor="sha256">SHA256:</label>
-        <br />
-        <textarea
-          rows={2}
-          className={textareaStyles}
-          readOnly={true}
-          value={hash ? uint8ArrayToHex(hash) : ''}
-        ></textarea>
-        <br />
-        <button id="sign" className={buttonStyles} onClick={onSignClick}>
-          Sign
-        </button>
-      </p>
-      <p className="pt-10">
-        <label htmlFor="public_key">Public Key:</label>
-        <br />
-        <textarea
-          rows={2}
-          className={textareaStyles}
-          readOnly={true}
-          value={publicKey ? uint8ArrayToHex(publicKey) : ''}
-        ></textarea>
-        <br />
-        <label htmlFor="signature">Signature:</label>
-        <br />
-        <textarea
-          rows={4}
-          className={textareaStyles}
-          readOnly={true}
-          value={signature ? uint8ArrayToHex(signature) : ''}
-        ></textarea>
-        <br />
-        <button id="verify" className={buttonStyles} onClick={onVerifyClick}>
-          Verify
-        </button>
-        <span className="pl-2">{verifyMessage}</span>
-      </p>
-      <p className="text-red-400">{error}</p>
+      <div className="flex flex-col items-start w-96">
+        <h1 className="text-xl pb-4">Threshold ECDSA Signature Demo</h1>
+        <FirebaseAuth />
+        {user && <Signature />}
+      </div>
     </main>
   );
 }
---
+++ icp/signature/src/signature_frontend/src/hooks/useFirebaseAuth.ts
@@ -0,0 +1,58 @@
+import { useState, useEffect } from 'react';
+import {
+  signInWithPopup,
+  signOut,
+  GoogleAuthProvider,
+  User,
+  onAuthStateChanged,
+} from 'firebase/auth';
+import { auth } from '@/firebaseConfig';
+
+export const googleProvider = new GoogleAuthProvider();
+
+auth.languageCode = 'ja';
+
+export const useFirebaseAuth = () => {
+  const [authErr, setAuthErr] = useState('');
+  const [user, setUser] = useState<User | null>();
+  const [idToken, setIdToken] = useState<string | null>();
+
+  const login = async () => {
+    try {
+      setAuthErr('');
+      await signInWithPopup(auth, googleProvider);
+    } catch (err: any) {
+      setAuthErr(err.message);
+    }
+  };
+
+  const logout = async () => {
+    try {
+      setAuthErr('');
+      await signOut(auth);
+    } catch (err: any) {
+      setAuthErr(err.message);
+    }
+  };
+
+  useEffect(() => {
+    const unregister = onAuthStateChanged(auth, (user) => {
+      setUser(user);
+      if (auth.currentUser) {
+        auth.currentUser.getIdToken(true).then((idToken) => {
+          setIdToken(idToken);
+          console.log('IdToken:', idToken);
+        });
+      }
+    });
+    return unregister;
+  });
+
+  return {
+    authErr,
+    login,
+    logout,
+    user,
+    idToken,
+  };
+};
