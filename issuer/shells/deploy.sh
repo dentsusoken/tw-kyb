ba059eea6810b7fa8ff40302ebafd146696e7b77
“ryohei-ebisawa” <xebisawa.ryohei@contract.isid.co.jp>
2024-03-22 09:31:05 +0900

    Fix Dockerfile

--- issuer/shells/deploy.sh
+++ issuer/shells/deploy.sh
@@ -2,15 +2,17 @@
 
 echo Deploy Start
 
-cp -f /workspace/resources/pom.xml /workspace/java-oauth-server
-cp -f /workspace/resources/appengine-web.xml /workspace/java-oauth-server/src/main/webapp/WEB-INF
-cp -f /workspace/resources/authlete.properties /workspace/java-oauth-server/src/main/webapp
+echo "デプロイするGCPプロジェクトのプロジェクトIDを入力してください。"
+read input
+sudo cp -f /workspace/resources/pom.xml /workspace/java-oauth-server
+sudo cp -f /workspace/resources/appengine-web.xml /workspace/java-oauth-server/src/main/webapp/WEB-INF
+sudo cp -f /workspace/resources/authlete.properties /workspace/java-oauth-server/src/main/webapp
 
-ln -sf  /usr/share/zoneinfo/Asia/Tokyo /etc/localtime
+sudo ln -sf  /usr/share/zoneinfo/Asia/Tokyo /etc/localtime
 version=`date +"%Y%m%dt%H%M%S"`
 
 cd /workspace/java-oauth-server/
 mvn clean package
-mvn package appengine:deploy -Dapp.deploy.projectId=tw-kyb -Dapp.deploy.version=${version}
+mvn package appengine:deploy -Dapp.deploy.projectId=${input} -Dapp.deploy.version=${version}
 
 echo Deploy Completed
--- issuer/shells/dev.sh
+++ issuer/shells/dev.sh
@@ -2,7 +2,7 @@
 
 echo Local Server Start
 
-cp -f /workspace/resources/authlete.properties /workspace/java-oauth-server
+sudo cp -f /workspace/resources/authlete.properties /workspace/java-oauth-server
 
 cd /workspace/java-oauth-server/
 mvn jetty:run
